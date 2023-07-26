6e157fbc6022b3065f8609f18a98654ec19ffa49
lancebenedictos <lancebenedictos@gmail.com>
2023-07-26 02:07:43 -0400

    navbar spacing fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="h-[50px] fixed top-0 bg-[#FCFD96] z-50 mix-blend-luminosity w-full flex border-b-2 border-black">
-      <div className="w-full lg:w-[80%] mx-auto flex justify-around items-center">
+      <div className="w-full lg:w-[80%] mx-auto flex justify-between items-center">
         <span className="flex items-center gap-2 select-none">
           <Image src="./me.svg" width={50} height={50} alt="my icon" />
           <p>Meh.</p>
